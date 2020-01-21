a58b81eb8d8807d67538d748887f0e768920be5b
Ron Terrence SJ. Bondoc <rtsbondoc@microsourcing.local>
2020-01-21 18:11:14 +0800

    Building structure for register and login view pt.1

--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
+#User service Controller
+
+def RegisterUser(request):
+    #Check if there is post request
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            #Save form
+            form.save()
+            messages.success(request, "User successfully created.")
+            return redirect('index')
+        else:
+            messages.error(request, "Error encountering registration. Please try again.")
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'html/registeruser.html', {'form': form})
